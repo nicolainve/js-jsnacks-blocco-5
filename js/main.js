646fe25fbf7d3ba4776f15d8942e384041ed5458
Nicola <nginvernizzi1@gmail.com>
2020-11-02 15:47:43 +0100

    jsnack 2 done

--- js/main.js
+++ js/main.js
@@ -40,3 +40,29 @@ var context = {
 var html = template(context);
 
 $('.container').append(html);
+
+
+/**
+ * JSnack 2
+    Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+    Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
+    Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
+*/
+
+var drivers = ['Vettel', 'Raikkonen', 'Hamilton', 'Leclerc', 'Bottas', 'Verstappen'];
+
+var tot = drivers.length - 1;
+
+var min = parseInt( prompt('Inserisci un numero tra 0 e ' + tot) );
+
+var max = parseInt( prompt('Inserisci un numero tra ' + min + ' e ' + tot) );
+
+var newArray = [];
+
+for ( var i = 0; i < drivers.length; i++ ) {
+    if (min <= i && max >= i) {
+        newArray.push(drivers[i]);
+    }
+};
+
+$('#drivers').text(newArray);
